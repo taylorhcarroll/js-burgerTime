2a98460d32852eb6c9d2618f68695770baa7cbb9
Taylor Carroll <taylorcarroll@Taylors-MacBook-Pro.local>
2019-08-27 14:39:34 -0500

    submitting burgerTime

---
+++ src/script/main.js
@@ -0,0 +1,49 @@
+//Your task is to iterate over the array of foods and invoke the function for 
+//each item so that the cookedFood array contains all of the items after they are cooked.
+
+const hamburger = {
+	name: 'Hamburger',
+	type: 'beef',
+	cooked: false,
+}
+const zucchini = {
+	name: 'Zucchini',
+	type: 'vegetable',
+	cooked: false,
+}
+const chickenBreast = {
+	name: 'Chicken Breast',
+	type: 'chicken',
+	cooked: false,
+}
+const corn = {
+	name: 'Corn',
+	type: 'vegetable',
+	cooked: false,
+}
+const steak = {
+	name: 'Steak',
+	type: 'beef',
+	cooked: false,
+}
+
+// An array that is grouping the objects together.
+const foods = [hamburger, zucchini, chickenBreast, corn, steak];
+
+// An empty array that will store the objects after the `grill()` function cooks the food.
+const cookedFood = [];
+
+function grill (currentObject) {
+    // Modify the food so that it is cooked
+    currentObject.cooked = true;
+
+    // Put the cooked food into the appropriate array
+    cookedFood.push(currentObject);
+};
+
+//giving instructions to the cook to pass foods through grill then to plate
+foods.forEach(element => {
+    grill(foods)
+}),
+//getting a receipt for the cooked foods
+console.log("Foods cooked:", cookedFood)
